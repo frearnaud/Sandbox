f2befb39b3797143b8eb6eb90163901acdba490a
frearnaud <frearnaud@users.noreply.github.com>
2020-10-21 11:01:29 +0200

    Update script.js

--- Misc/1-Bin2Dec/script.js
+++ Misc/1-Bin2Dec/script.js
@@ -1,23 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   const input = document.getElementById("inDigits");
+
   input.addEventListener("keyup", function (e) {
     let val = input.value;
     let elResult = document.getElementById("result");
 
-    if (e.key === "1" || e.key === "0") {
-      let result = 0;
-      const tab = val.split("");
+    console.log(e);
 
-      for (i = 0; i < tab.length; i++) {
-        const index = tab.length - i - 1;
-        console.log(index);
-        if (tab[i] === "1") result = result + Math.pow(2, index);
-      }
-
-      elResult.innerText = result;
-    } else {
+    if (e.key !== "1" && e.key !== "0" && e.key !== "Backspace") {
       val = val.substring(0, val.length - 1);
       input.value = val;
     }
+
+    let result = 0;
+    const tab = val.split("");
+
+    for (i = 0; i < tab.length; i++) {
+      const index = tab.length - i - 1;
+      console.log(index);
+      if (tab[i] === "1") result = result + Math.pow(2, index);
+    }
+
+    elResult.innerText = result;
   });
 });
